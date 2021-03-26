93a421e51acf048fdbdb445e26932544490aed6c
Ian Fajardo <ianfajardozx@gmail.com>
2021-03-26 15:23:40 -0400

    add mobile links page

---
+++ pages/links.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import Navigation from "../components/nav";
+import Footer from "../components/footer";
+import Link from "next/link";
+import Date from "../components/date";
+import { getSortedPostsData } from "../lib/posts";
+
+const recentPostscount = 4;
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData(recentPostscount);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Links({ allPostsData }) {
+  return (
+    <div>
+      <div className="bg-secondary">
+        <div className="container text-white">
+          <div className="row">
+            <div className="col-lg-12">
+              <h1 className="h4 font-weight-bolder text-primary mt-3">
+                Try Ortho Keys!
+              </h1>
+              <p style={{ fontSize: "14px" }}>
+                Ortholinear keyboards are computer keyboards with their keys
+                arranged in a grid layout. It can be a fun DIY project to
+                assemble your own keyboard, add your own switches, and program
+                your own keymappings.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          {allPostsData.map(({ slug, date, title, description, image }) => (
+            <div className="col-lg-12" key={slug}>
+              <div className="mb-2">
+                <div className="text-muted ">
+                  <small>
+                    <Date dateString={date} />
+                  </small>
+                </div>
+                <Link href={`/posts/${slug}`}>
+                  <a className="">{title}</a>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>{" "}
+      <Footer />
+    </div>
+  );
+}
