5f50e075d6bcbfa627c1d1c1d166db332f1a684e
Ian Fajardo <ianfajardozx@gmail.com>
2021-07-30 12:33:20 -0400

    update configure with sidebar content

--- pages/configure.js
+++ pages/configure.js
@@ -57,11 +57,57 @@ export default function Configuring() {
       </div>
 
       <div className="section-container ">
-        <div className="container">
-          <div className="card ele-2">
-            <div className="px-5 py-5 py-lg-5">
-              <div className="row align-items-center">
-                <div className="col-lg-12">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="d-none d-lg-block d-xl-block col-lg-2">
+              <h3 className="font-weight-bold text-primary">Keyboards</h3>
+              <div className="mt-3">
+                <div className="card">
+                  <div className="card-body">
+                    <h3>OLKB Plank &amp; Preonic</h3>
+                    <img
+                      className="img-fluid my-3"
+                      src="/plank-preonic.jpg"
+                      alt="OLKB Planck &amp; Preonic"
+                    />
+                    <p className="mt-3">
+                      OLKB is the producer of the highly popular Planck &amp;
+                      Preonic Ortholinear keyboards. The owner, Jack Humbert, is
+                      also the founder of QMK. These keyboards are sold on the
+                      OLKB website and frequently on Drop.com
+                    </p>
+                    <a href="/olkb" className="btn btn-secondary mt-3">
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-3">
+                <div className="card">
+                  <div className="card-body">
+                    <h3>NIU40 / NIU Mini</h3>
+                    <img
+                      className="img-fluid"
+                      src="/niu40.webp"
+                      alt="NIU 40 NIU Mini"
+                    />
+                    <p>
+                      The NIU 40 / NIU Mini is a 40% keyboard similar to the
+                      Planck ortholinear keyboard. You purchase them a DIY kit
+                      at <a href="https://kbdfans.com/r?id=eft22v">KBDFans</a>.
+                      A bit cheaper than the OLKB offerings, it offers similar
+                      functionality and features.
+                    </p>
+                    <a href="/niu40" className="btn btn-secondary mt-3">
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-8 col-md-8">
+              <div className="card ele-2">
+                <div className="px-5 py-5 py-lg-5">
                   <h2 className="font-weight-bold text-primary">
                     QMK Configurator & Toolbox
                   </h2>
@@ -158,7 +204,8 @@ export default function Configuring() {
                       Buy Planck on Amazon
                     </a>
 
-                    <br/><br/>
+                    <br />
+                    <br />
 
                     <a
                       href="https://drop.com/buy/preonic-mechanical-keyboard?utm_source=linkshare&referer=T93XGG"
@@ -172,6 +219,57 @@ export default function Configuring() {
                       href="https://amzn.to/3xzTDbF"
                     >
                       Buy Preonic on Amazon
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-4">
+              <h3 className="font-weight-bold text-primary">Blog</h3>
+              <div className="mt-3">
+                <div className="card">
+                  <div className="card-img-top">
+                    <a
+                      className="lead"
+                      href="/posts/what-to-do-after-getting-an-ortholinear-keyboard"
+                    >
+                      <img
+                        className="img-fluid mb-3"
+                        src="/first-ortholinear-board.png"
+                        alt="What to do after getting your first Ortholinear Keyboard"
+                      />
+                    </a>
+                  </div>
+                  <div className="card-body">
+                    <a
+                      className="lead"
+                      href="/posts/what-to-do-after-getting-an-ortholinear-keyboard"
+                    >
+                      What to do after getting your first Ortholinear Keyboard
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-3">
+                <div className="card">
+                  <div className="card-img-top">
+                    <a
+                      className="lead"
+                      href="/posts/budget-keycaps-for-your-ortho-keyboard"
+                    >
+                      <img
+                        className="img-fluid mb-3"
+                        src="/best-budget-keycaps-ortho-keyboard.png"
+                        alt="Best Budget Keycaps for Your Ortho Keyboard"
+                      />
+                    </a>
+                  </div>
+                  <div className="card-body">
+                    <a
+                      className="lead"
+                      href="/posts/budget-keycaps-for-your-ortho-keyboard"
+                    >
+                      Best Budget Keycaps for Your Ortho Keyboard
                     </a>
                   </div>
                 </div>
