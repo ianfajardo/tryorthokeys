4e94fa7000c3e9e94e4891b8517245bf1381e179
ianfajardo <ianfajardozx@gmail.com>
2021-09-18 16:56:32 -0400

    links added for instagram

--- pages/links.js
+++ pages/links.js
@@ -3,34 +3,20 @@ import Navigation from "../components/navigation";
 import Footer from "../components/footer";
 import Link from "next/link";
 import Date from "../components/date";
-import { getSortedPostsData } from "../lib/posts";
 
-const recentPostscount = 6;
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData(recentPostscount);
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Links({ allPostsData }) {
+export default function Links() {
   return (
-    <div>
+    <div className="ortho-links">
       <div className="bg-secondary">
         <div className="container text-white">
           <div className="row">
-            <div className="col-lg-12">
+            <div className="col-lg-12 text-center">
+              <div className="link-image"></div>
               <h1 className="h4 font-weight-bolder text-primary mt-3">
-                Try Ortho Keys!
+                @tryorthokeys
               </h1>
               <p style={{ fontSize: "14px" }}>
-                Ortholinear keyboards are computer keyboards with their keys
-                arranged in a grid layout. It can be a fun DIY project to
-                assemble your own keyboard, add your own switches, and program
-                your own keymappings.
+                Ortholinear keyboard guides, news and more.
               </p>
             </div>
           </div>
@@ -38,35 +24,44 @@ export default function Links({ allPostsData }) {
       </div>
       <div className="container mt-3">
         <div className="row">
-          {allPostsData.map(({ slug, date, title, description, image }) => (
-            <div className="col-lg-12" key={slug}>
-            <div className="mb-5">
-              <div className="card">
-                <div className="card-img-top">
-                  <Link href={`/posts/${slug}`}>
-                    <a className="lead">
-                      <img
-                        className="img-fluid mb-3"
-                        src={image}
-                        alt={title}
-                      />
-                    </a>
-                  </Link>
-                </div>
-                <div className="card-body">
-                  <div className="text-muted ">
-                    <small>
-                      <Date dateString={date} />
-                    </small>
-                  </div>
-                  <Link href={`/posts/${slug}`}>
-                    <a className="lead">{title}</a>
-                  </Link>
-                </div>
-              </div>
-            </div>
+          <div className="col-lg-12">
+            <a
+              href="https://www.tryorthokeys.com/keycaps"
+              className="btn btn-primary btn-lg btn-block mb-3"
+            >
+              Ortholinear Keycap List
+            </a>
+            <a
+              href="https://amzn.to/2VRlx4W"
+              className="btn btn-primary btn-lg btn-block mb-3"
+            >
+              Planck - Compact 40% Ortholinear Keyboard
+            </a>
+            <a
+              href="https://www.tryorthokeys.com/posts/budget-keycaps-for-your-ortho-keyboard"
+              className="btn btn-primary btn-lg btn-block mb-3"
+            >
+              Best Budget Keycaps for Your Ortho Keyboard
+            </a>
+            <a
+              href="https://www.tryorthokeys.com/posts/configuring-your-planck-preonic"
+              className="btn btn-primary btn-lg btn-block mb-3"
+            >
+              Configuring your Planck or Preonic with QMK Firmware{" "}
+            </a>
+            <a
+              href="https://www.tryorthokeys.com/keyboards"
+              className="btn btn-primary btn-lg btn-block mb-3"
+            >
+              Learn about Ortho Keyboards
+            </a>
+            <a
+              href="https://www.tryorthokeys.com/posts"
+              className="btn btn-primary btn-lg btn-block mb-3"
+            >
+              Blog
+            </a>
           </div>
-          ))}
         </div>
       </div>{" "}
       <Footer />
