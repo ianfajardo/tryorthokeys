c017f94248590652d271679484ea1b9c5fc11a4c
ianfajardo <ianfajardozx@gmail.com>
2021-09-18 17:10:09 -0400

    update button size and container for ig links

--- pages/links.js
+++ pages/links.js
@@ -22,48 +22,57 @@ export default function Links() {
           </div>
         </div>
       </div>
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-lg-12">
-            <a
-              href="https://www.tryorthokeys.com/keycaps"
-              className="btn btn-primary btn-lg btn-block mb-3"
-            >
-              Ortholinear Keycap List
-            </a>
-            <a
-              href="https://amzn.to/2VRlx4W"
-              className="btn btn-primary btn-lg btn-block mb-3"
-            >
-              Planck - Compact 40% Ortholinear Keyboard
-            </a>
-            <a
-              href="https://www.tryorthokeys.com/posts/budget-keycaps-for-your-ortho-keyboard"
-              className="btn btn-primary btn-lg btn-block mb-3"
-            >
-              Best Budget Keycaps for Your Ortho Keyboard
-            </a>
-            <a
-              href="https://www.tryorthokeys.com/posts/configuring-your-planck-preonic"
-              className="btn btn-primary btn-lg btn-block mb-3"
-            >
-              Configuring your Planck or Preonic with QMK Firmware{" "}
-            </a>
-            <a
-              href="https://www.tryorthokeys.com/keyboards"
-              className="btn btn-primary btn-lg btn-block mb-3"
-            >
-              Learn about Ortho Keyboards
-            </a>
-            <a
-              href="https://www.tryorthokeys.com/posts"
-              className="btn btn-primary btn-lg btn-block mb-3"
-            >
-              Blog
-            </a>
+      <div className="ortho-link-container">
+        <div className="container mt-3">
+          <div className="row">
+            <div className="col-lg-12">
+              <a
+                href="https://www.tryorthokeys.com/keycaps"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Ortholinear Keycap List
+              </a>
+              <a
+                href="https://amzn.to/2VRlx4W"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Planck - Compact 40% Ortholinear Keyboard
+              </a>
+              <a
+                href="https://amzn.to/2VRlx4W"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Planck - Compact 40% Ortholinear Keyboard
+              </a>
+              <a
+                href="https://www.tryorthokeys.com/posts/budget-keycaps-for-your-ortho-keyboard"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Best Budget Keycaps for Your Ortho Keyboard
+              </a>
+              <a
+                href="https://www.tryorthokeys.com/posts/configuring-your-planck-preonic"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Configuring your Planck or Preonic with QMK Firmware{" "}
+              </a>
+              <a
+                href="https://www.tryorthokeys.com/keyboards"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Learn about Ortho Keyboards
+              </a>
+              <a
+                href="https://www.tryorthokeys.com/posts"
+                className="btn btn-primary btn-lg btn-block mb-3"
+              >
+                Blog
+              </a>
+            </div>
           </div>
-        </div>
-      </div>{" "}
+        </div>{" "}
+      </div>
+
       <Footer />
     </div>
   );
