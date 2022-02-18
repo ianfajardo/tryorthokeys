91ac5aaad70c5f5273eef28887679575be3bd351
ianfajardo <ianfajardozx@gmail.com>
2022-02-17 22:05:17 -0500

    fix posts redirect

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/post/:slug*',
+        source: '/posts/:slug*',
         destination: '/blog/:slug*', // Matched parameters can be used in the destination
         permanent: true,
       },
